90882f4e38d581d379d638c6dba699b861647e3a
Delivion93 <artem.abramov.1993@gmail.com>
2025-11-22 11:58:01 +0100

    sheet01 ex01

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import Card from "./contents/card";
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Card></Card>
+      <Card />
     </>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,20 @@
+function Card() {
+  return (
+    <>
+      <div className="card">
+        <img
+          className="image"
+          src="https://animalpark.com.ua/files_ci/153/kalita-3.jpg"
+          alt="Parrot"
+        />
+        <div className="info">
+          <h1>Name: Coco</h1>
+          <h1>Surname: Drilo</h1>
+          <h2>Job: Laughing with Artem</h2>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Card;
